c1ae4b63c83be1c1e0b29dfaf58e5dd3be227a4f
Dmytro Uglach <dmitryuglach@gmail.com>
2024-10-31 14:09:17 +0200

    parameters

--- main.py
+++ main.py
@@ -8,13 +8,23 @@ client = OpenAI()
 # for model in client.models.list():
 #     print(model)
 
-system_role_content = ('Format output the way where the max line length is 80 characters')
+system_role_content = ('You are helpful assistance')
 
 response = client.chat.completions.create(
     model='gpt-3.5-turbo',
     messages=[
         {'role': 'system', 'content': system_role_content},
-        {'role': 'user', 'content': 'Please tell me a short English story'},
-    ]
+        {'role': 'user', 'content': 'Write a short article about Norse Mythology gods'},
+    ],
+    temperature=1,
+    # seed=1234
+    # top_p=0.1,
+    # max_tokens=10,
+    # n=2,
+    # stop=[';', '.'],
+    frequency_penalty=0,  # between -2 and 2, default 0
+    presence_penalty=0,  # between -2 and 2, default 0
+
 )
+
 print(response.choices[0].message.content)
